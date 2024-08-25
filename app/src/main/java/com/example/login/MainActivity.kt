b3365af5db0034fe4e54db80b7738ea8887007c5
Maxi <maxitiero2023@gmail.com>
2024-08-25 19:53:42 -0300

    Primera version

--- app/src/main/java/com/example/login/MainActivity.kt
+++ app/src/main/java/com/example/login/MainActivity.kt
@@ -1,18 +1,18 @@
 package com.example.login
-
+import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
+import android.view.View
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    lateinit var nombreUsuario:TextView
-    lateinit var contraseña:TextView
-    lateinit var ingresar:Button
-    lateinit var registrarse:Button
+
+    private val usuarioValido = "Juan Torres"
+    private val passwordValida = "1234utn"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,11 +23,44 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        nombreUsuario=findViewById(R.id.nameUserText)
-        contraseña=findViewById(R.id.passwordText)
-        ingresar=findViewById(R.id.registrarsePantalla)
-        registrarse=findViewById(R.id.)
     }
 
-    public void irIngresar(View view)
+    fun irIngresar(view: View) {
+        val inputUsuario = findViewById<EditText> (R.id.nameUserL)
+        val inputPassword = findViewById<EditText>(R.id.contrasenaL)
+        val usuarioIngresado = inputUsuario.text.toString()
+        val passwordIngresada = inputPassword.text.toString()
+
+        if (usuarioIngresado == usuarioValido && passwordIngresada == passwordValida) {
+            // Usuario y contraseña correctos, iniciar sesión
+            val intent = Intent(this, BienvenidoActivity::class.java)
+            startActivity(intent)
+        } else {
+            // Usuario o contraseña incorrectos, mostrar error
+            //Toast.makeText(this, "Nombre de usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show()
+            //val rootView = findViewById<View>(android.R.id.content)
+            //Snackbar.make(rootView, "Datos incorrectos", Snackbar.LENGTH_SHORT).show()
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Error")
+            builder.setMessage("Nombre de usuario o contraseña incorrectos")
+            builder.setPositiveButton("Aceptar", null)
+            val dialog: AlertDialog = builder.create()
+            dialog.show()
+        }
+    }
+
+    fun irRegistrarse(view: View) {
+        val intent = Intent(this, RegistrarseActivity::class.java)
+        startActivity(intent)
+    }
+
+
+
+
+
+
+
+
+
+
 }
--- app/src/main/java/com/example/login/RegistrarseActivity.kt
+++ app/src/main/java/com/example/login/RegistrarseActivity.kt
@@ -1,6 +1,8 @@
 package com.example.login
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,31 @@ class RegistrarseActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val correoEditText = findViewById<EditText>(R.id.correoElectronico)
+        val nameUserEditText = findViewById<EditText>(R.id.nameUserR)
+        val registrarseBtn = findViewById<Button>(R.id.registrarseR)
+
+        registrarseBtn.setOnClickListener {
+            // Validar que los campos no estén vacíos
+            val nameUser = nameUserEditText.text.toString().trim()
+            val correo = correoEditText.text.toString().trim()
+
+
+            if (nameUser.isEmpty()) {
+                nameUserEditText.error = "El nombre es obligatorio"
+                nameUserEditText.requestFocus()
+            }
+            if (correo.isEmpty()) {
+                correoEditText.error = "El correo electrónico es obligatorio"
+                correoEditText.requestFocus()
+            }
+            // else {
+                // Si ambos campos están llenos, continuar con el registro
+               // Toast.makeText(this, "Usuario registrado", Toast.LENGTH_SHORT).show()
+                // Aquí va la lógica para registrar al usuario
+            }
+        }
     }
-}
+
+
